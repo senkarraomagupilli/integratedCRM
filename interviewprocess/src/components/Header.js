243bc02a8a8654935949f890870ce1559de48936
senkarraomagupilli <senkarrao.magupilli@gigleaz.com>
2024-05-17 13:11:13 +0530

    first commit

---
+++ interviewprocess/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand"><b>Interview Process</b></a>
+                    <form class="d-flex" role="search">
+
+                        <i class="bi bi-person-circle"></i>
+
+                    </form>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
